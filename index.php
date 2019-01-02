46dd830eefe569a112de0346ab49ff7b30953d03
moustapha95000 <moussss1234@gmail.com>
2019-01-02 14:30:00 +0100

    correspondance des modifs

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GIT</title>
+</head>
+<body>
+    <h1>Premier titre GIT</h1>
+    <ul>
+        <li>liste</li>
+        <li>liste</li>
+        <li>liste</li>
+        <li>liste</li>
+        <li>liste</li>
+    </ul>
+    
+</body>
+</html>
